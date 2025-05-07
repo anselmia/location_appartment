89da2bc55ddeb1baef165a76cbc2b93d65dda247
foxnono06 <anselmi.arnaud@yahoo.fr>
2025-05-07 18:06:48 +0200

    update eriodic task schedule def

--- location_site/production.py
+++ location_site/production.py
@@ -87,10 +87,3 @@ CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
-
-CELERY_BEAT_SCHEDULE = {
-    "airbnb-sync-every-30-min": {
-        "task": "logement.tasks.test_airbnb_sync",
-        "schedule": crontab(minute="*/30"),
-    },
-}
