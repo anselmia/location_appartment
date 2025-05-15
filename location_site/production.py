a67fa199448249fec13ea454ac43d22041f3ada9
foxnono06 <anselmi.arnaud@yahoo.fr>
2025-05-15 13:15:44 +0200

    celery confg

--- location_site/production.py
+++ location_site/production.py
@@ -144,6 +144,7 @@ CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 ADMINS = [("Arnaud Anselmi", "anselmi.arnaud@yahoo.fr")]
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
