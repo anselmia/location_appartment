518ebb734d50a3161892c64a3058150e535df793
foxnono06 <anselmi.arnaud@yahoo.fr>
2025-05-15 02:11:05 +0200

    log task

--- logement/tasks.py
+++ logement/tasks.py
@@ -8,11 +8,6 @@ from logement.models import Logement, airbnb_booking, booking_booking, Reservati
 
 # Setup a logger
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-handler = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 
 
 def process_calendar(url, source):
