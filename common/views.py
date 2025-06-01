f51d961f447493a2cc5eea51ae8a3dfd275c6a9c
foxnono06 <anselmi.arnaud@yahoo.fr>
2025-06-01 08:18:37 +0200

    trailing comma

--- common/views.py
+++ common/views.py
@@ -10,7 +10,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.urls import reverse,
+from django.urls import reverse
 from django. shortcuts import redirect
 from django.contrib import messages
 
